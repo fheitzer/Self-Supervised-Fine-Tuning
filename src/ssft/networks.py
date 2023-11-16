3a7ab74d352ca609382e16934d6bfcdadaf292f2
stfrheitz <friedrich.heitzer@stud.h-da.de>
2023-11-16 18:57:39 +0100

    Add call function majority vote with data collection

--- src/ssft/networks.py
+++ src/ssft/networks.py
@@ -10,11 +10,13 @@ HEIGHT = 450
 
 class Ensemble(Model):
     """
-
+    One network to rule them all
     """
 
-    def __init__(self, models):
+    def __init__(self,
+                 models: List[tf.keras.Model] = None):
         super(Ensemble, self).__init__()
+        assert models is not None, "Must be initialized with list of models"
         self.models = models
         self.continuous_training_data = None
         self.missed_data = None
@@ -27,41 +29,79 @@ class Ensemble(Model):
         self.missed_data_spec = (tf.TensorSpec(shape=(WIDTH, HEIGHT, 3), dtype=tf.float64),
                                  tf.TensorSpec(shape=(2,), dtype=tf.float32))
 
-    def __call__(self, x):
+    def __call__(self,
+                 data,
+                 voting: str = 'soft',
+                 collect: bool = False):
         """
         Make prediction using a majority voting
         :param args:
         :param kwargs:
         :return:
         """
-        predictions = list()
-        for model in self.models:
-            pred = np.argmax(model.predict(y))
+        model_predictions = np.asarray([model.predict(data).numpy() for model in self.models])
+        ensemble_predictions = list()
+        if voting == 'soft':
+            # Get the average prediction for every datapoint and class. (Datapoint, Class)
+            ensemble_predictions = np.mean(model_predictions, axis=0)
+        if voting == 'hard':
+            cllt_datapoints = list()
+            cllt_labels = list()
+            # Get the hard labels by each model. return shape = (Model, datapoint)
+            hard_model_predictions = np.argmax(model_predictions, axis=-1)
+            # Iterate over hard label list for each datapoint
+            for i, datapoint_preds in enumerate(np.transpose(hard_model_predictions)):
+                # Get the unique classes voted for an how often
+                unique, counts = np.unique(datapoint_preds, return_counts=True, axis=-1)
+                # Unanimous decision
+                if len(unique) == 1:
+                    # Make decision
+                    ensemble_predictions.extend(np.eye(self.num_classes)[unique[0]])
+                    # Dont Collect data
 
+                # Majority Vote
+                if np.max(counts) >= (len(self.models) / 2):
+                    # Make decision
+                    majority_vote = unique[np.argmax(counts)]
+                    ensemble_predictions.extend(np.eye(self.num_classes)[majority_vote])
+                    # Collect data
+                    if collect:
+                        cllt_datapoints.extend(x[i])
+                        cllt_labels.extend(majority_vote)
 
-        return prediction
+                # No majority
+                else:
+                    # Make dummy decision
+                    # Collect miss
+                    self.collect_miss(data)
 
-    def fit(self, dataset=None):
+        return ensemble_predictions
+
+    def fit(self, epochs:int):
         """
         Fit the models of the ensemble to a dataset or to the continuous training data collected by the ensemble
         :param dataset:
         :return:
         """
-        if dataset is None:
-            self.fit(self.continuous_training_data)
-        pass
+        ###### DOESNT WORK AS WE OLY COLLECT IMAGE AND LABEL
+        assert dataset is not None, "self.continuous_training_data is None." \
+                                    " Please review a dataset to collect datapoints"
+        # For every model filter out relevant datapoints and fit it to them
+        for i, model in enumerate(self.models):
+            model.fit(self.continuous_training_data.filter(lambda im, pred, model, lbl: model == i),
+                      epochs=epochs)
 
-    def review_and_collect_data(self, dataset):
+    def review_and_collect(self, dataset):
         """
         Make predictions on a dataset and collect continuous training data
         :param dataset:
         :return:
         """
-        predictions, models = self(dataset, black_box=False)
+        predictions, models = self(dataset, return_type='')
 
         pass
 
-    def cycle_offline(self, data, num_cycles):
+    def cycle_offline(self, data, num_cycles, epochs_per_cycle):
         """
         Cycle over dataset. In each cycle review and collect data, and then fit the ensemble models on continuous
         training data. Repeat cycles and hope for performance increase on passed dataset
@@ -69,20 +109,18 @@ class Ensemble(Model):
         :return:
         """
         for i in num_cycles:
-            self.review_and_collect_data(data)
-            self.fit()
+            self.review_and_collect(data)
+            self.fit(epochs=epochs_per_cycle)
             self.reset_data()
 
-    def collect_continuous_training_data(self, x, pred, wrong_model, label):
+    def collect_continuous_training_data(self, x, pred):
         """Add the current datapoint to self.data
         with the index of the model that needs to be trained on that datapoint
         and the label predicted by the other networks.
         """
         img = tf.data.Dataset.from_tensor_slices(x)
-        pred = tf.data.Dataset.from_tensor_slices([pred]).map(lambda x: tf.one_hot(x, self.num_classes))
-        wrong_model = tf.data.Dataset.from_tensor_slices([wrong_model])
-        label = tf.data.Dataset.from_tensor_slices(label)
-        datapoint = tf.data.Dataset.zip((img, pred, wrong_model, label))
+        pred = tf.data.Dataset.from_tensor_slices(pred)
+        datapoint = tf.data.Dataset.zip((img, pred))
         if self.continuous_training_data is None:
             self.continuous_training_data = datapoint
         else:
@@ -93,8 +131,6 @@ class Ensemble(Model):
         Review by hand later.
         """
         img = tf.data.Dataset.from_tensor_slices([x])
-        label = tf.data.Dataset.from_tensor_slices(y)
-        datapoint = tf.data.Dataset.zip((img, label))
         if self.missed_data is None:
             self.missed_data = datapoint
         else:
