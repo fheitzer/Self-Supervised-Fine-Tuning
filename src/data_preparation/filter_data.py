e55f93431ed7abb507ce636f70311dd4983ff8d8
stfrheitz <friedrich.heitzer@stud.h-da.de>
2023-11-30 11:27:44 +0100

    Add concat meta function

--- src/data_preparation/filter_data.py
+++ src/data_preparation/filter_data.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
 import pandas as pd
 
 WIDTH = 650
@@ -284,5 +284,75 @@ def concat_all_tfds():
     ds.save('data/Isic_2020/Train/tf/isic_all', compression='GZIP')
 
 
+def move_files():
+    # filter_isic2019_train(start=15000, end=None, name='15k_to_end')
+    data_dir = '/Users/floyd/Documents/Studium/DS/FreiesProjekt/Self Supervised Fine Tuning/data'
+    meta = pd.read_csv(f"{data_dir}/metadata/processed_UDA1.csv")
+    # files = [file for file in os.listdir() if file.endswith('.png')]
+
+    for _, row in meta.iterrows():
+        image_id = row['image_id']
+        label = row['dx']
+        if type(label) is not str:
+            continue
+        if 'nevus' in label or 'nv' in label:
+            os.rename(f'{data_dir}/UDA/UDA1/600x450/{image_id}.png', f'{data_dir}/UDA/UDA/nv/{image_id}.png')
+        if 'mel' in label:
+            os.rename(f'{data_dir}/UDA/UDA1/600x450/{image_id}.png', f'{data_dir}/UDA/UDA/mel/{image_id}.png')
+
+
+def move_files_dir():
+    dataset_dir = '/Users/floyd/Documents/Studium/DS/FreiesProjekt/Self Supervised Fine Tuning/data/VIENNA/ISIC2020_VIENNA1/600x450'
+    data_dir = '/Users/floyd/Documents/Studium/DS/FreiesProjekt/Self Supervised Fine Tuning/data'
+    meta = pd.read_csv(f"{data_dir}/metadata/processed_UDA2.csv")
+    files = [file for file in os.listdir(dataset_dir) if file.endswith('.png')]
+
+    for image_id in files:
+        label = meta[meta['image_id'] == image_id[:-4]]['dx'].item()
+        if type(label) is not str:
+            continue
+        if 'nevus' in label or 'nv' in label:
+            os.rename(f'{data_dir}/UDA/UDA2/600x450/{image_id}', f'{data_dir}/UDA/UDA/nv/{image_id}')
+        if 'mel' in label:
+            os.rename(f'{data_dir}/UDA/UDA2/600x450/{image_id}', f'{data_dir}/UDA/UDA/mel/{image_id}')
+
+
+def cat_meta(name: str):
+    df = pd.DataFrame()
+    dir = "/Users/floyd/Documents/Studium/DS/FreiesProjekt/Self Supervised Fine Tuning/data/metadata"
+    for file in os.listdir(dir):
+        if name in file:
+            file_path = os.path.join(dir, file)
+            part = pd.read_csv(file_path)
+            df = pd.concat([df, part])
+    df.to_csv(os.path.join(dir, name))
+
+
 if __name__ == "__main__":
-    filter_isic2019_train(start=15000, end=None, name='15k_to_end')
+    cat_meta("UDA")
+    exit()
+    #filter_isic2019_train(start=15000, end=None, name='15k_to_end')
+    dataset_dir = '/Users/floyd/Documents/Studium/DS/FreiesProjekt/Self Supervised Fine Tuning/data/DERM7PT/600x450'
+    data_dir = '/Users/floyd/Documents/Studium/DS/FreiesProjekt/Self Supervised Fine Tuning/data'
+    meta = pd.read_csv(f"{data_dir}/metadata/processed_DERM7PT.csv")
+    files = [file for file in os.listdir(dataset_dir) if file.endswith('.png')]
+
+    for image_id in files:
+        label = meta[meta['image_id'] == image_id[:-4]]['dx'].item()
+        if type(label) is not str:
+            continue
+        if 'nevus' in label or 'nv' in label:
+            os.rename(f'{data_dir}/DERM7PT/600x450/{image_id}', f'{data_dir}/DERM7PT/nv/{image_id}')
+        if 'mel' in label:
+            os.rename(f'{data_dir}/DERM7PT/600x450/{image_id}', f'{data_dir}/DERM7PT/mel/{image_id}')
+
+    # for _, row in meta.iterrows():
+    #     image_id = row['image_id']
+    #     label = row['dx']
+    #     if type(label) is not str:
+    #         continue
+    #     if 'nevus' in label or 'nv' in label:
+    #         os.rename(f'{data_dir}/UDA/UDA1/600x450/{image_id}.png', f'{data_dir}/UDA/UDA/nv/{image_id}.png')
+    #     if 'mel' in label:
+    #         os.rename(f'{data_dir}/UDA/UDA1/600x450/{image_id}.png', f'{data_dir}/UDA/UDA/mel/{image_id}.png')
+
